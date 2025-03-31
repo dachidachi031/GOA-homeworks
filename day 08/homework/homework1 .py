ea6b529930abde8ce8c6b45b5ed350748a5b1694
dachidachi031 <irinafifia83@gmail.com>
2025-03-31 21:50:27 +0400

    commit

---
+++ day 08/homework/homework1 .py
@@ -0,0 +1,4 @@
+num1 =int(input("enter mother age "))
+num2 = int(input("enter father age "))
+print(num1 + 25)
+print(num2 + 25)
---
+++ day 08/homework/homework3 .py
@@ -0,0 +1,3 @@
+num1 = int(input("enter your age"))
+num1_str = str(num1)
+print(num1_str)
